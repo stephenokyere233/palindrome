5990b1f21e154b83f1ad2a10240f0935115bebaa
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2022-07-29 17:39:27 +0000

    Add files via upload

--- palindrome.js
+++ palindrome.js
@@ -1,39 +1,32 @@
-// const inputData=document.querySelector('#input');
-const btn= document.querySelector('button');
+const btn = document.querySelector("button");
 const form = document.getElementById("addform");
 var section = document.querySelector("section");
-// var p = document.createElement("p");
 var p = document.querySelector("#new-p");
-// section.append(p);
-var str=document.querySelector('#input').value;
-const palindrome=(e,str)=>{
-    e.preventDefault();
-    str = document.querySelector('#input').value;
-
-  var check = str.split("").reverse().join("");
-
-// //   for (let i = 0; i < mainsplit.length; i++) {
-//     if (main[i] == " ") {
-//       check.splice(i,1);
-
-//     //   console.log(i);
-//     } else {
-//       continue;
-//     }
-//   }
- 
-  var output;
-  if (str.toLowerCase() === check.toLowerCase()) {
-   
-    p.textContent = `${str} is a palindrome`;
-  } else {
-   
-    p.textContent= `${str} is not a palindrome`;
+var str = document.querySelector("#input").value;
+const palindrome = (e, str) => {
+  e.preventDefault();
+  str = document.querySelector("#input").value;
+  if(str!=" " &&str!= ""){
+  var split = str.split("");
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] == " ") {
+      split.splice(i, 1);
+    }
   }
 
-  console.log(output.toUpperCase());
- 
+  var strjoin = split.join("");
+  var reverse = split.reverse();
+  var check = reverse.join("");
+
+  if (strjoin.toLowerCase() === check.toLowerCase()) {
+    p.textContent = `${str} is a palindrome`;
+  } else {
+    p.textContent = `${str} is not a palindrome`;
+  }
+  }
+  else{
+     p.textContent = `Invalid Input`;
+  }
 
 };
-form.addEventListener('submit',palindrome);
-
+form.addEventListener("submit", palindrome);
